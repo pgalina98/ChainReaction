26bf89df660ea886459e3a4b2c2b9db389098f8c
Patrik Galina <patrik.galina@king-ict.hr>
2022-06-26 21:34:26 +0200

    chore: Refactor Head component into _document.tsx file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,10 +12,6 @@ function App({ Component, pageProps }: AppProps) {
     <React.Fragment>
       <Head>
         <title>ChainReaction</title>
-        <link
-          rel="stylesheet"
-          href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css"
-        ></link>
       </Head>
 
       <Header />
