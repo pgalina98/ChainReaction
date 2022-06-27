bd2a4d40358cf5f79786984d3c1f28835f9be98c
Patrik Galina <patrik.galina@king-ict.hr>
2022-06-27 22:11:05 +0200

    chore: Animate login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 
+import { motion } from "framer-motion";
+
 import { Button, Card, Icon, Input } from "common/components";
 
 import { InptType } from "common/types/inputy-type";
@@ -18,9 +20,22 @@ const Login = () => {
 
   return (
     <div className={`${styles.h_full} grid grid-cols-2 gap-4`}>
-      <div
+      <motion.div
+        initial={{ opacity: 0, x: "-100%" }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.5 }}
         className={`${styles.div_rounded_50_0_32} ${styles.left_content} p-8 relative`}
       >
+        <motion.div
+          animate={{
+            scale: [1, 2, 2, 2, 1],
+            rotate: [0, 0, 360, 360, 0],
+            borderRadius: ["25%", "25%", "50%", "50%", "50%"],
+          }}
+          className={`absolute right-0 -mr-5 p-2 text-4xl rounded-full flex justify-center content-center ${styles.icon_arrow} ${styles.animation_bounce}`}
+        >
+          <i className="las la-chevron-circle-right" />
+        </motion.div>
         <div className="flex justify-center">
           <p className="font-light text-5xl">Sign In</p>
         </div>
@@ -32,8 +47,13 @@ const Login = () => {
             height={550}
           />
         </div>
-      </div>
-      <div className="ml-40 mr-40 mt-28">
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.6, duratin: 0.5 }}
+        className="ml-40 mr-40 mt-28"
+      >
         <Card className="w-full">
           <Input
             label="Username"
@@ -70,12 +90,30 @@ const Login = () => {
         </Card>
         <Card className="w-full mt-8 p-1">
           <div className="flex justify-center">
-            <Icon icon="lab la-facebook-f" color="facebook" hoverable />
-            <Icon icon="lab la-google-plus-g" color="google" hoverable />
-            <Icon icon="lab la-instagram" color="instagram" hoverable />
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 1, duratin: 0.15 }}
+            >
+              <Icon icon="lab la-facebook-f" color="facebook" hoverable />
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 1.25, duratin: 0.15 }}
+            >
+              <Icon icon="lab la-google-plus-g" color="google" hoverable />
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 1.55, duratin: 0.15 }}
+            >
+              <Icon icon="lab la-instagram" color="instagram" hoverable />
+            </motion.div>
           </div>
         </Card>
-      </div>
+      </motion.div>
     </div>
   );
 };
