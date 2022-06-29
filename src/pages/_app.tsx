744d49383852c944dfc0ed0dcc14599b79b8335a
Patrik Galina <patrik.galina@king-ict.hr>
2022-06-29 21:47:28 +0200

    chore: Add AnimatePresence as parent component of each component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,13 @@ import React from "react";
 import Head from "next/head";
 import type { AppProps } from "next/app";
 
+import { AnimatePresence } from "framer-motion";
+
 import { Header } from "common/components";
 
 import "common/styles/globals.scss";
 
-function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps, router }: AppProps) {
   return (
     <React.Fragment>
       <Head>
@@ -15,7 +17,9 @@ function App({ Component, pageProps }: AppProps) {
       </Head>
 
       <Header />
-      <Component {...pageProps} />
+      <AnimatePresence exitBeforeEnter>
+        <Component {...pageProps} key={router.pathname} />
+      </AnimatePresence>
     </React.Fragment>
   );
 }
